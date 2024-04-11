17212e1572503d6e8506c7c932f0088f52b8a07b
PavelSergeevichYurevich <Pasha2028@icloud.com>
2024-04-11 13:56:51 +0700

    врод как работает ордеритем

--- app/schemas/order_schema.py
+++ app/schemas/order_schema.py
@@ -1,11 +1,6 @@
-from typing import Dict, List
 from pydantic import BaseModel
-
-from schemas.order_item_schema import OrderItem
 
 class OrderCreateSchema(BaseModel):
     customer_id: int
     status: str
-    item: list[OrderItem]
-    """ class Config:
-        from_attributes = True """
+    
