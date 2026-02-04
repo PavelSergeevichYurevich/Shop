cc10fa7747314b37c007ef3456e576e3191b1963
PavelSergeevichYurevich <Pasha2028@icloud.com>
2026-02-04 16:10:00 +0700

    cover 95%

---
+++ tests/test_item.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+from sqlalchemy import select
+from app.models.models import Item
+
+def test_show_items_200(client, test_db):
+    item = Item(
+        name='Iphone 17 PRO',
+        description='Iphone 17 PRO',
+        price=100,
+        quantity=10,
+        category='Cell phones'
+        
+    )
+    test_db.add(item)
+    test_db.commit()
+    test_db.refresh(item)
+    
+    item = Item(
+        name='Iphone 17 PRO MAX',
+        description='Iphone 17 PRO MAX',
+        price=100,
+        quantity=10,
+        category='Cell phones'
+        
+    )
+    test_db.add(item)
+    test_db.commit()
+    test_db.refresh(item)
+    
+    response = client.get('/item/show/')
+    assert response.status_code == 200
+    items = response.json()
+    assert isinstance(items, list)
+    assert len(items) == 2
+    names = {item['name'] for item in items}
+    categories = {item['category'] for item in items}
+    assert 'Iphone 17 PRO' in names
+    assert 'Iphone 17 PRO MAX' in names
+    assert 'Cell phones' in categories
+    
+def test_add_item_200(client, test_db):
+    response = client.post('/item/add/', params={
+        'name': 'Iphone 17 PRO',
+        'description': 'Iphone 17 PRO',
+        'price': 100,
+        'quantity': 10,
+        'category': 'Cell phones'
+        },
+        files={'file': ('test.jpg', b'fake-bytes', 'image/jpeg')}                
+    )
+    assert response.status_code == 201
+    data = response.json()
+    assert data['name'] == 'Iphone 17 PRO'
+    assert data['category'] == 'Cell phones'
+    assert data['price'] == 100
+    assert data['quantity'] == 10
+    stmnt = select(Item).where(Item.name == 'Iphone 17 PRO')
+    result = test_db.execute(stmnt).scalar_one_or_none()
+    assert result is not None
+    assert result.image != ''
+    assert 'static/images' in result.image
+    
+def test_delete_item_200(client, test_db, tmp_path):
+    path = tmp_path / 'test.jpg'
+    path.write_bytes(b'fake-image-content')
+    item = Item(
+        name='Iphone 17 PRO MAX',
+        description='Iphone 17 PRO MAX',
+        image=str(path),
+        price=100,
+        quantity=10,
+        category='Cell phones'
+        
+    )
+    test_db.add(item)
+    test_db.commit()
+    test_db.refresh(item)
+    assert path.exists()
+    response = client.delete(f'/item/delete/{item.id}')
+    assert response.status_code == 200
+    assert response.json()['message'] == f'Товар {item.id} успешно удален'
+    stmnt = select(Item).where(Item.id == item.id)
+    result = test_db.execute(stmnt).scalar_one_or_none()
+    assert result is None
+    assert not path.exists()
+    
+    
+def test_delete_item_not_exists_404(client, test_db):
+    response = client.delete('/item/delete/999999')
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'Товар не найден'
+    
+    
+    
--- tests/test_order.py
+++ tests/test_order.py
@@ -1,3 +1,4 @@
+from pytest import MonkeyPatch
 from sqlalchemy import select
 from app.models.models import Customer, Item, Order, OrderItem
 
@@ -404,6 +405,161 @@ def test_add_item_quantity_not_enough_400(client, test_db):
     stmnt = select(OrderItem).where(OrderItem.order_id == order_id, OrderItem.item_id == item.id)
     result = test_db.execute(stmnt).scalar_one_or_none()
     assert result is None
+    
+def test_show_orders_with_no_orders_200(client, test_db):
+    customer = Customer(
+        name=NAME,
+        hashed_password=PASSWORD,
+        email=EMAIL
+    )
+    test_db.add(customer)
+    test_db.commit()
+    test_db.refresh(customer)
+    
+    response = client.get(f'/order/show/{customer.id}')
+    assert response.status_code == 200
+    assert response.json() == []
+    
+def test_show_orders_200(client, test_db):
+    customer = Customer(name=NAME, 
+                        email=EMAIL, 
+                        hashed_password=PASSWORD
+    )
+    test_db.add(customer)
+    test_db.commit()
+    test_db.refresh(customer)
+    
+    item = Item(
+        name='Iphone 17 PRO',
+        description='Iphone 17 PRO',
+        price=100,
+        quantity=10,
+        category='Cell phones'
+        
+    )
+    test_db.add(item)
+    test_db.commit()
+    test_db.refresh(item)
+    
+    response = client.post('/order/add/', json={
+        'order_data': {
+            'customer_id': customer.id,
+            'status': 'pending'
+        },
+        'items_data': [{
+            
+                'item_id': item.id,
+                'quantity': 2
+            }]
+    })
+    assert response.status_code == 201
+    order_id = response.json()['id']
+    
+    response = client.get(f'/order/show/{customer.id}')
+    assert response.status_code == 200
+    orders = response.json()
+    assert len(orders) > 0
+    order_ids = {order['id'] for order in orders}
+    assert order_id in order_ids
+    
+def test_add_order_with_item_not_exist_404(client, test_db):
+    customer = Customer(name=NAME, 
+                        email=EMAIL, 
+                        hashed_password=PASSWORD
+    )
+    test_db.add(customer)
+    test_db.commit()
+    test_db.refresh(customer)
+    
+    response = client.post('/order/add/', json={
+        'order_data': {
+            'customer_id': customer.id,
+            'status': 'pending'
+        },
+        'items_data': [{
+            
+                'item_id': 999999,
+                'quantity': 2
+            }]
+    })
+    
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'Товар 999999 не найден'
+    
+def test_order_update_item_not_found_404(client, test_db):
+    response = client.put('/order/update/', json={
+        "order_id": 999999, 
+        "item_id": 999999, 
+        "new_quantity": 1
+    })
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'Позиция в заказе не найдена'
+    
+def test_create_order_wrong_500(client, test_db, monkeypatch):
+    response = client.post('/order/add/', json={
+        'order_data': {
+            'customer_id': 99999,
+            'status': 'pending'
+        },
+        'items_data': []
+    })
+    assert response.status_code == 500
+    assert response.json()['detail'] == 'Ошибка при создании заказа'
+    
+def test_item_not_found_404(client, test_db, monkeypatch):
+    customer = Customer(name=NAME, 
+                        email=EMAIL, 
+                        hashed_password=PASSWORD
+    )
+    test_db.add(customer)
+    test_db.commit()
+    test_db.refresh(customer)
+    
+    item = Item(
+        name='Iphone 17 PRO',
+        description='Iphone 17 PRO',
+        price=100,
+        quantity=10,
+        category='Cell phones'
+        
+    )
+    test_db.add(item)
+    test_db.commit()
+    test_db.refresh(item)
+    
+    response = client.post('/order/add/', json={
+        'order_data': {
+            'customer_id': customer.id,
+            'status': 'pending'
+        },
+        'items_data': [{
+            
+                'item_id': item.id,
+                'quantity': 2
+            }]
+    })
+    assert response.status_code == 201
+    order_id = response.json()['id']
+    item_id = item.id
+    db = test_db
+    original_get = db.get
+    def fake_get(model, ident, *args, **kwargs):
+        if model is OrderItem:
+            return original_get(model, ident, *args, **kwargs)  # реальный OrderItem
+        if model is Item:
+            return None  # имитируем "товар исчез"
+        return original_get(model, ident, *args, **kwargs)
+    monkeypatch.setattr(db, 'get', fake_get)
+
+    response = client.put('/order/update/', json={
+        'order_id': order_id,
+        'item_id': item_id,
+        'new_quantity': 2
+    })
+    
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'Товар не найден на складе'
+    
     
 
     
